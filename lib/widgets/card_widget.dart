8a72d9a607879aec1457f2d777229b9472600e47
Deyson Venté <deiandresvent@hotmail.com>
2024-12-04 22:04:45 -0500

    imagen de fondo para el card

--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -6,6 +6,7 @@ import '../models/joke_model.dart';
 class JokeCard extends StatefulWidget {
   const JokeCard({super.key, required this.joke});
   final Joke joke;
+  final String chuckImage ="https://4.bp.blogspot.com/-Y-ncAf3VUTA/VBfuQnJeCGI/AAAAAAAAA70/O4CN676kxYU/s1600/158277069_076e03.jpg";
   @override
   State<JokeCard> createState() => _JokeCardState();
 }
@@ -22,40 +23,53 @@ class _JokeCardState extends State<JokeCard> {
         margin: const EdgeInsets.all(10),
         child: Padding(
           padding: const EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: NetworkImage(
-                  widget.joke.iconUrl
-                ),
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(image: NetworkImage(
+                widget.chuckImage,
               ),
-              const SizedBox(height: 10,),
-              Center(
-                child: Text(
-                  widget.joke.value,
-                  style: GoogleFonts.aBeeZee(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(const Color.fromARGB(255, 28, 84, 105), BlendMode.dstOver)
+              )
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 20,
+                  backgroundImage: NetworkImage(
+                    widget.joke.iconUrl
                   ),
                 ),
-              ),
-              const SizedBox(height: 20,),
-              Text(
-                'Fecha Creación: ${widget.joke.createdAt}',
-                style: GoogleFonts.roboto(
-                  fontSize: 14,
+                const SizedBox(height: 10,),
+                Center(
+                  child: Text(
+                    widget.joke.value,
+                    style: GoogleFonts.aBeeZee(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    ),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20,),
-              Text(
-                'Fecha Actualización: ${widget.joke.updatedAt}',
-                style: GoogleFonts.roboto(
-                  fontSize: 14,
+                const SizedBox(height: 50,),
+                Text(
+                  'Fecha Creación: ${widget.joke.createdAt}',
+                  style: GoogleFonts.roboto(
+                    fontSize: 18,
+                      color: const Color.fromARGB(255, 249, 253, 2)
+                  ),
                 ),
-              )
-            ],
+                const SizedBox(height: 20,),
+                Text(
+                  'Fecha Actualización: ${widget.joke.updatedAt}',
+                  style: GoogleFonts.roboto(
+                    fontSize: 18,
+                      color: const Color.fromARGB(255, 2, 247, 234)
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
